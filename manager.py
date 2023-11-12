6847d098718b31fc9807b963604ef8e23bf9d978
Alejandro Castilla Quesada <contacto@alejandro-castilla.com>
2023-11-12 12:05:06 +0100

    Create a chart with electricity prices and some fixes
    * Use smtplib to send emails. "mail" command does not seem to handle MIME messages well because it adds a break line after the headers.
    * Re-initialize plug auth protocol when something fails.
    * Improve HTML for the email with the prices.

--- manager.py
+++ manager.py
@@ -1,21 +1,50 @@
 import configparser
-import subprocess
+import smtplib
 import time
 import requests
 import logging
+import matplotlib.pyplot as plt
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
 from datetime import datetime, timedelta
-from PyP100 import PyP100
+from PyP100 import PyP100, auth_protocol
 
 FIRST_PERIOD_OFF_DELAY = timedelta(hours=2).total_seconds()
 SECOND_PERIOD_OFF_DELAY = timedelta(hours=1).total_seconds()
+CHART_FILE_NAME = "electricity_prices_chart.png"
+
+SMTP_SERVER = smtplib.SMTP('localhost')
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def send_email(subject, message, from_email, to_email):
-    command = (f"echo '{message}' | mail -s '{subject}' -a 'From: Energy Manager "
-               f"<{from_email}>' {to_email}")
-    subprocess.run(command, shell=True)
+def send_email(subject, content, from_email, to_email, attach_chart=False):
+    mime_message = MIMEMultipart("related")
+    mime_message["From"] = f"Energy manager <{from_email}>"
+    mime_message["To"] = f"Alejandro Castilla Quesada <{to_email}>"
+    mime_message["Subject"] = subject
+
+    html_body = f"""
+    <!DOCTYPE html>
+    <html>
+    <body>
+    {content}
+    {"<br><img src='cid:chart'>" if attach_chart else ""}
+    </body>
+    </html>
+    """
+
+    mime_text = MIMEText(html_body, "html", _charset="utf-8")
+    mime_message.attach(mime_text)
+
+    if attach_chart:
+        with open(CHART_FILE_NAME, "rb") as f:
+            chart = MIMEImage(f.read())
+        chart.add_header("Content-ID", "<chart>")
+        mime_message.attach(chart)
+
+    SMTP_SERVER.sendmail(from_email, to_email, mime_message.as_string())
 
 
 if __name__ == '__main__':
@@ -28,8 +57,6 @@ if __name__ == '__main__':
     tapo_password = config.get('credentials', 'tapo.password')
 
     plug = PyP100.Switchable(tapo_address, tapo_email, tapo_password, "old")
-    plug.handshake()
-    plug.login()
 
     target_date = None
     target_hour_first_period = None
@@ -69,10 +96,13 @@ if __name__ == '__main__':
                         [(hour, price) for hour, price in hourly_prices if 14 <= hour <= 23],
                         key=lambda x: x[1])
 
-                    email_message = f"💶🔋 Electricity prices for {datetime.now().date()}:\n\n"
+                    email_message = f"<p>💶🔋 Electricity prices for {datetime.now().date()}:</p>"
 
                     for hour, price in hourly_prices:
-                        email_message += f"⏱️💶 {hour}h: {price} €/MWh\n"
+                        email_message += f"⏱️💶 {hour}h: {price} €/MWh"
+                        email_message += "<br>"
+
+                    email_message += "<p>"
 
                     cheapest_hour_message_first_period = (
                         f"⬇️💶 Cheapest hour within first period: {target_hour_first_period[0]}h - "
@@ -81,11 +111,21 @@ if __name__ == '__main__':
                         f"⬇️💶 Cheapest hour within second period: {target_hour_second_period[0]}h - "
                         f"{target_hour_second_period[1]} €/MWh")
 
-                    email_message += "\n" + cheapest_hour_message_first_period + "\n"
-                    email_message += cheapest_hour_message_second_period + "\n"
+
+                    email_message += cheapest_hour_message_first_period
+                    email_message += "<br>"
+                    email_message += cheapest_hour_message_second_period
+
+                    email_message += "</p>"
+
+                    plt.bar([hour for hour, price in hourly_prices], [price for hour, price in hourly_prices])
+                    plt.xlabel("Hour")
+                    plt.ylabel("Price (€/MWh)")
+                    plt.title("Electricity prices")
+                    plt.savefig(CHART_FILE_NAME)
 
                     send_email(f'Electricity prices for {target_date.date()}', email_message,
-                               manager_from_email, manager_to_email)
+                               manager_from_email, manager_to_email, True)
 
                     logging.info(f"Successfully downloaded prices data for {target_date.date()}. "
                                  f"Email with all data has been sent.")
@@ -109,5 +149,17 @@ if __name__ == '__main__':
                                    manager_from_email, manager_to_email)
                 except Exception as e:
                     logging.error(f"Failed to enable plug: {e}")
+                    # Try to re-initialize the protocol
+                    if isinstance(plug.protocol, auth_protocol.OldProtocol):
+                        # WARNING: session must be re-initialized because the plug does not seem to allow more than
+                        # one handshake in the same session.
+                        # See https://github.com/fishbigger/TapoP100/issues/62#issuecomment-1107876214
+                        try:
+                            plug.protocol.session = requests.Session()
+                            plug.protocol.Initialize()
+                        except Exception as e:
+                            logging.error(f"Failed to re-initialize plug protocol: {e}")
+
+                        logging.info("Successfully re-initialized plug protocol")
 
         time.sleep(30)
